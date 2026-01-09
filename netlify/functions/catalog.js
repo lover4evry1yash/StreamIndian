948c90823bb93aa0520d47c3dd5afdb8ea94f6c0
lover4evry1yash <lover4evry1.yash@gmail.com>
2026-01-10 00:15:11 +0530

    Update catalog.js
    Add multiple catalog static items

--- netlify/functions/catalog.js
+++ netlify/functions/catalog.js
@@ -1,7 +1,27 @@
 export const handler = async (event) => {
   const parts = event.path.split('/');
   const type = parts[parts.length - 2];
-  const id = parts[parts.length - 1].replace('.json', '');
+
+  const metas = [
+    {
+      id: "streamindian:movie1",
+      type,
+      name: "RRR",
+      poster: "https://m.media-amazon.com/images/M/MV5BM2Y5Y2U3NzktZTE1OC00YjU4LTg2N2ItY2QzZTU5NWI4ZWI4XkEyXkFqcGc@._V1_.jpg"
+    },
+    {
+      id: "streamindian:movie2",
+      type,
+      name: "KGF Chapter 1",
+      poster: "https://m.media-amazon.com/images/M/MV5BN2Y0ZmUxZWMtZGU5NS00OWE5LWE4MjQtZDRmYmE3ZTM3OWFjXkEyXkFqcGc@._V1_.jpg"
+    },
+    {
+      id: "streamindian:movie3",
+      type,
+      name: "Baahubali: The Beginning",
+      poster: "https://m.media-amazon.com/images/M/MV5BN2RmM2NmZjctNDQ1Ni00Y2Y0LTg0OGYtZDJjYzZmNjg5Y2I2XkEyXkFqcGc@._V1_.jpg"
+    }
+  ];
 
   return {
     statusCode: 200,
@@ -9,15 +29,6 @@ export const handler = async (event) => {
       "Content-Type": "application/json",
       "Access-Control-Allow-Origin": "*"
     },
-    body: JSON.stringify({
-      metas: [
-        {
-          id: "streamindian:test",
-          type,
-          name: "StreamIndian Test Item",
-          poster: "https://via.placeholder.com/300x450?text=StreamIndian"
-        }
-      ]
-    })
+    body: JSON.stringify({ metas })
   };
 };
