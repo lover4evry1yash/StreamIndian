7edb92460a4b0668748541434efd4b74fd35a57b
Yash <lover4evry1.yash@gmail.com>
2026-01-11 23:05:34 +0530

    Fix AI homescreen module and utils

---
+++ src/utils/response.js
@@ -0,0 +1,10 @@
+export function json(data, status = 200, headers = {}) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      ...headers
+    }
+  })
+}
