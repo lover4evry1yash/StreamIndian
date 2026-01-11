b6c57c4541860fea2b7850dd281100fa242098b0
Yash <lover4evry1.yash@gmail.com>
2026-01-12 00:09:15 +0530

    Fix manifest response headers for Stremio compatibility

--- worker.js
+++ worker.js
@@ -8,6 +8,8 @@ import { handleMetaEpisode } from './src/meta/episode.js'
 import { handleStream } from './src/streams/torrent.js'
 import { handleAIHome } from './src/ai/homescreen.js'
 import { handleAISearch } from './src/ai/search.js'
+import { json } from './src/utils/response.js'
+
 
 const router = Router()
 
@@ -23,18 +25,31 @@ router.options('*', () =>
 )
 
 
-router.get('/manifest.json', () => Response.json({
-  id: 'org.streamindian',
-  version: '2.0.0',
-  name: 'StreamIndian',
-  description: 'Premium Indian OTT-style Stremio addon',
-  resources: ['catalog', 'meta', 'stream'],
-  types: ['movie', 'series'],
-  catalogs: [
-    { type: 'movie', id: 'streamindian.movies', name: 'Movies', extraSupported: ['skip', 'search'] },
-    { type: 'series', id: 'streamindian.series', name: 'Series', extraSupported: ['skip', 'search'] }
-  ]
-}))
+router.get('/manifest.json', () =>
+  json({
+    id: 'org.streamindian',
+    version: '2.0.0',
+    name: 'StreamIndian',
+    description: 'Premium Indian OTT-style Stremio addon',
+    resources: ['catalog', 'meta', 'stream'],
+    types: ['movie', 'series'],
+    catalogs: [
+      {
+        type: 'movie',
+        id: 'streamindian.movies',
+        name: 'Movies',
+        extraSupported: ['skip', 'search']
+      },
+      {
+        type: 'series',
+        id: 'streamindian.series',
+        name: 'Series',
+        extraSupported: ['skip', 'search']
+      }
+    ]
+  })
+)
+
 
 router.get('/catalog/movie/:id.json', handleCatalogMovies)
 router.get('/catalog/series/:id.json', handleCatalogSeries)
